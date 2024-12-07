250cc5adef6db944c0db9e426d8e50712519ec49
Meet-bhuva <meetbhuva96241@gmail.com>
2024-12-07 20:00:28 +0000

    Add datatype

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,9 @@
+let num = 123 // number datatype
+let name = "Meet"//string data-type
+let ans = true//boolean data-type true/false
+let val = null //null data-type, null mean empty that user did not send value
+let add ; //undefine data-type
+
+console.log(typeof "Hitesh")
+console.log(typeof null)//data-type is null but type is object
+console.log(typeof undefined) //data-type is undefined and type is undefined
