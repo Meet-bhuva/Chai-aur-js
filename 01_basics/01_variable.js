59b9deac0479825884f046fc51e7c23d03b023ac
Meet-bhuva <meetbhuva96241@gmail.com>
2024-12-07 19:13:25 +0000

    Add variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 123456; //const is a constant keyword 
+let accountName = "meet-patel" // when 2 types of declaring variable 1-let and 2-var
+var accountPassword = "123456"// var changed in scope that's why do not use var //scope mean = {}
+accountCity = "surat-super" //do not use ever
+let accountState; //undefine value 
+ 
+
+console.log(accountId)
+
+console.table([accountId, accountName, accountPassword,accountCity])
