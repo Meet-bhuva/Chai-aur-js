4879e16f7914541e1e74e8c65c6029ea8af2f9c7
Meet-bhuva <meetbhuva96241@gmail.com>
2024-12-07 20:31:25 +0000

    dd operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let value = "33"
+
+console.log(typeof value)
+console.log(typeof (value))
+
+let valueInNumber = Number(value)
+console.log(typeof valueInNumber)
+
+//33 = 33 - 
+//"33abc" = NaN -not a number
+//trule = 1
+//false = 0
+//null = 0
+
+
+let isLogged = 1
+// 1 => true, 0 => false, "Me" => true, "" => false
+let checkedValue = Boolean(isLogged)
+console.log(checkedValue);
+
+let stringNumb = String(isLogged)
+console.log(typeof stringNumb)
