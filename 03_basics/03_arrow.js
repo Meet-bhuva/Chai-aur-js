73df0b4f65d2c004e5021a05d1c9005b67e84d05
Meet-bhuva <meetbhuva96241@gmail.com>
2025-02-27 20:09:31 +0000

    cbf

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -5,8 +5,15 @@ const user = {
 
     welcomeMessage : function(){
         console.log(`${this.userName}, welcome to my repo`) //this keyword is used to bring current context, from 1 to 9 line is current context 
+        // console.log(this)// This keyword give evry value from the their context 
     }
 }
-user.welcomeMessage()
-user.userName = "bansuManiya"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.userName = "bansuManiya"
+// user.welcomeMessage()
+
+// console.log(this) //output is {} because of body has a object
+
+
+
+//*************** I can not use this keyword in normal function, use only in Arrow function and object */
