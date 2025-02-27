26456e409b9abb95fe806573cc465d74dbe06906
Meet-bhuva <meetbhuva96241@gmail.com>
2025-02-27 18:37:57 +0000

    changed

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -12,8 +12,40 @@ if (true) {    //block scope
     let a = 10; //let do not exit from if scope
     const b = 20; //const do not exit from if scope
     // var c = 30; // When var exit from if scope
-    console.log("INNER", a)
+    // console.log("INNER", a)
 }
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c) // output is 30, so do not use var variable because it get value from if scope
+
+
+
+
+
+//Parent chaild pase thi ice_cream no mangi sake jyare child parent pase thi mangi sake
+function one(){   //Parent function
+    const username = "MeetPatel"
+
+    function two(){ //Child function
+        const websiteName = "Toms-Globe"
+        // console.log(username) //Child function can use value of parent function but parent can not use.
+    }
+    // console.log(websiteName)
+
+    two()
+}
+one()
+
+
+
+if (true){
+    const userName = "BansuManiya"
+
+    if(userName === "BansuManiya"){
+        const webSite = " On instagram.com"
+        console.log( userName + webSite);
+    }
+
+    // console.log(webSite);
+}
+// console.log(userName)
