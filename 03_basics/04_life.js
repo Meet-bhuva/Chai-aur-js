921b03db19d3c2bac4622b479f68ee0626c11c96
Meet-bhuva <meetbhuva96241@gmail.com>
2025-02-28 09:51:47 +0000

    changed

--- 03_basics/04_life.js
+++ 03_basics/04_life.js
@@ -1,1 +1,10 @@
 // Imediately Invoked Function Expression (IIFE)
+
+(function chai(){ //Named IIFE
+    console.log("DB CONNECTED")
+})(); //use semi-column compulsory 
+
+((nem) => { //Unamed IIFE
+    console.log(`DB CONNECTED ${name}`)
+})("meet")
+//()() here used 2 parenthiesis, 1st is used for function defination and 2nd is used for function execution
