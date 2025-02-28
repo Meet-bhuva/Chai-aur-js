979727630e3cc8e0be059f3d3230809883512733
Meet-bhuva <meetbhuva96241@gmail.com>
2025-02-28 13:44:49 +0000

    all changed

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,55 @@
+// if
+
+let isUserloggedIn =1;
+// if(isUserloggedIn){
+//     console.log("meetBhuva")
+// }else{
+//     console.log("undefine")
+// }
+// condition are <, >, <=, >=, ==, !=, ===
+
+
+// if(isUserloggedIn){
+//     let power = "fly"
+//     console.log(`My power is ${power}`)
+// }
+// console.log(`My power is ${power}`)
+
+
+/********* SHORT-HAND NOTATION ****************************************/
+// let balance = 1000;
+// if(balance > 500) console.log("Executive")
+
+
+/********* NESTING ****************************************/
+// let balance = 1000;
+
+// if(balance < 500){
+    //     console.log("less than 500")
+    // }else if(balance < 750){
+        //     console.log("less than 750")
+        // }else if(balance < 900){
+            //     console.log("less than 900")
+            // }else{
+                //     console.log("grater than 900")
+                // }
+
+
+/*************************** CONDITION ********************************/
+
+let userLogged = true;
+let debitCard = true;
+let creditCard = true;
+let userLoggedFromMail = false;
+
+// //&& checked every condition, if all is true so it execute
+// if(userLogged && debitCard && creditCard && 2==2){
+//     console.log("All condition executive")
+// }
+
+
+// // || checked any one condition, if it is true so it execute
+// if(userLogged || userLoggedFromMail){
+//     console.log("code is executive")
+// }
+
