904b9d52853ed7d2798919452453c683666d62b3
Meet-bhuva <meetbhuva96241@gmail.com>
2025-02-28 14:08:28 +0000

    changed

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,14 @@
+let month = 6;
+
+switch(month){
+    case 1 : console.log("January")
+    break;
+    case 2 : console.log("February")
+    break;
+    case 3 : console.log("March")
+    break;
+    case 4 : console.log("April")
+    break;
+    default: console.log("Do not find any value")
+    break;
+}
