122be14a45322cd50ee889ed0a736c4176171b89
Meet-bhuva <meetbhuva96241@gmail.com>
2025-02-28 14:47:53 +0000

    changed

---
+++ 04_control_flow/03_truthy_falsy.js
@@ -0,0 +1,20 @@
+const userEmail = "M@eetBhuva@gmail.com"
+
+if(userEmail){
+    console.log("Got user's email")
+}else{
+    console.log("can not find user email address")
+}
+
+//Falsy values : false, 0, -0, BigInt 0n, null, undefine, NaN
+//Truthy values : "0", 'false', " ", [], {}, function(){}
+
+const emptyArray = []
+if(emptyArray.length === 0){
+    console.log("Array is empty")
+}
+
+const emptyObject = {}
+if(Object.keys(emptyObject).length === 0){
+    console.log("Object is empty")
+}
