103b334500b0a0d9be6736c9c61a8b900ea865c4
Meet-bhuva <meetbhuva96241@gmail.com>
2025-03-01 07:19:21 +0000

    While loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,37 @@
+// While loop 
+
+// New variable 
+// while( condition ){
+// increasing var
+// }
+
+let index = 1;
+
+while(index <= 10){
+    // console.log(`Value is ${index}`)
+    index++;
+}
+
+
+let myArray = ["Meet", "om", "trushant", "parikshit"]
+let arr = 0;
+
+while (arr < myArray.length) {
+    // console.log(`My freiend name is ${myArray[arr]}`)
+    arr++;
+}
+
+
+
+/*************  Do - While loop ***************/
+
+// New variable 
+// do {
+// increasing var
+// }while( condition )
+
+let score = 1;
+do{
+    // console.log(`Value is ${score}`)
+    score++
+}while(score <= 10)
