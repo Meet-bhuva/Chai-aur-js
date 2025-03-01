401971bc56e21659f51afbdc609b1d66bcbe8995
Meet-bhuva <meetbhuva96241@gmail.com>
2025-03-01 20:08:21 +0000

    08

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,16 @@
+// let myNewnums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let myNumbers = myNewnums.map( (num) => num + 10);
+// console.log(myNumbers)
+
+
+/******************** Chaining *****************/
+
+let myNewnums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let myNumbers = myNewnums.map( (num) => num * 10 )
+                         .map( (num) => num+1)
+                         .filter( (num) => num >= 40)
+
+
+console.log(myNumbers)
